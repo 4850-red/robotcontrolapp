db469b7d0a86cf7e85cfba016f841174342b856f
Jack Young <jackyoung@jacaro.dev>
2022-11-22 22:49:45 -0500

    Implemented QR code scanning

--- navigation/screens/homeScreen.js
+++ navigation/screens/homeScreen.js
@@ -3,7 +3,9 @@ import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-nativ
 import { FontAwesome5 } from '@expo/vector-icons'; 
 import axios from 'axios';
 import IpContext from '../../state/IpContext';
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
+const reg = /http:\/\/(?<ip>\d+.\d+.\d+.\d+):50000/;
 
 //need to add connection to port as well
 
@@ -11,27 +13,101 @@ export default function HomeScreen({navigation}){
 
     const { ipAddress, setIpAddress } = React.useContext(IpContext)
 
+    const [hasPermission, setHasPermission] = React.useState(null);
+
+    const [scanned, setScanned] = React.useState(false);
+
+    const [success, setSuccess] = React.useState(false);
+
+    React.useEffect(() => {
+        (async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    });
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        if (type !== 256) {
+            alert("Invalid QR Code. Scan again.");
+            return;
+        }
+
+        const match = reg.exec(data)
+        if (match !== null && match !== undefined && match.groups !== null) {
+            setIpAddress(match.groups["ip"])
+            setSuccess(true);
+        } else {
+            alert("Invalid QR Code. Scan again.")
+        }
+    }
+
     function onChange(event) {
         setIpAddress(event);
+    }
+
+    function onRescan() {
+        setSuccess(false);
+        setScanned(false);
     }
 
     const connectPressed = () => {
         navigation.navigate('Remote');
     }
 
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.loginContainer}>
                 <FontAwesome5 name='robot' size={100} color='black'/>
-                <TextInput
+                { hasPermission === true && scanned === false &&
+                    <BarCodeScanner 
+                    onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                    style={StyleSheet.absoluteFill}
+                    />
+                }
+                { hasPermission === null &&
+                    <Text>Requesting for camera permission</Text>
+                }
+
+                { hasPermission === false &&
+                    <Text>No access to camera</Text>
+                }
+
+                { scanned == true && success == true &&
+                    <>
+                        <Text>Detected IP: { ipAddress }</Text>
+                        <TouchableOpacity style={styles.connectButton} onPress={connectPressed}>
+                            <Text style={styles.inputText}>CONNECT</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.connectButton} onPress={onRescan}>
+                            <Text style={styles.inputText}>RESCAN</Text>
+                        </TouchableOpacity>
+                    </>
+                }
+                {
+                    scanned == true && success == false &&
+
+                    <TouchableOpacity style={styles.connectButton} onPress={onRescan}>
+                        <Text style={styles.inputText}>RESCAN</Text>
+                    </TouchableOpacity>
+                }
+                
+                
+                
+                {/* <TextInput
                     style={styles.input}
                     placeholder='IP Address'
                     onChangeText={onChange}
                     >
                 </TextInput>
-                <TouchableOpacity style={styles.connectButton} onPress={connectPressed}>
-                    <Text style={styles.inputText}>CONNECT</Text>
-                </TouchableOpacity>
+                 */}
             </View>
         </View>
     );
@@ -67,6 +143,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#1063DE',
+        margin: 10,
         borderRadius: 10,
         width: 250,
         height: 50,
